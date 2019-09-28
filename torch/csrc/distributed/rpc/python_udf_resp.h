096bc87c0be6c282c48dcfd37690af3dd7ff6e0a
pritam <pritam@devvm756.atn1.facebook.com>
2019-09-28 16:13:40 -0700

    Add send and recv backward functions for builtin operators RPC.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25527
    
    Master GH issue: https://github.com/pytorch/pytorch/issues/23110.
    
    This change builds upon https://github.com/pytorch/pytorch/pull/24876 and
    provides all the autograd hooks needed for a forward pass with distributed rpc
    for builtin operators. This change does not address distributed rpc for python
    UDFs and that will be addressed in follow up PRs.
    
    Summary of changes:
    1. Attach send autograd functions when a request is sent from the client and
    response is sent from the server.
    2. Attach receive autograd functions when a request is received on the server
    and a response is received on the client.
    3. Generate a globally unique autograd_message_id for each send/recv autograd
    function pair to uniquely identify them.
    ghstack-source-id: 90989582
    
    Differential Revision: [D17148077](https://our.internmc.facebook.com/intern/diff/D17148077/)

---
+++ torch/csrc/distributed/rpc/python_udf_resp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <torch/csrc/distributed/rpc/rpc_command_base.h>
+
+namespace torch {
+namespace distributed {
+namespace rpc {
+
+// RPC call representing the response of a Python UDF over RPC.
+class TORCH_API PythonUDFResp final : public RpcCommandBase {
+ public:
+  explicit PythonUDFResp(std::vector<char> pickledPayload);
+
+  Message toMessage() && override;
+
+  static std::unique_ptr<PythonUDFResp> fromMessage(const Message& message);
+
+  const std::vector<char>& pickledPayload() const;
+
+ private:
+  std::vector<char> pickledPayload_;
+};
+
+} // namespace rpc
+} // namespace distributed
+} // namespace torch
